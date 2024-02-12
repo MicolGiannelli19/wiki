1c5d6544a3917c6b0e927b2bbf89f2f219fa5916
MicolGiannelli19 <micol.giannelli@outlook.com>
2024-02-12 09:55:11 +0000

    working on the search bar

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = "encyclopedia"
+
 urlpatterns = [
     path("", views.index, name="index"),
+    path("micla", views.micla, name="micla"),
+    path("search", views.search, name="search"),
     path("<str:title>", views.display, name="display"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from . import util
+from django import forms
+
+
+class NewSearchForm(forms.Form):
+    task = forms.CharField(label="Search")
 
 
 def index(request):
@@ -10,8 +15,35 @@ def index(request):
 
 
 def display(request, title):
+    # TODO: find the appropiate place to return an error if the entry does not exist
+    # TODO: If an entry is requested that does not exist, the user should be presented with an error page indicating that their requested page was not found.
+    # TODO: find out how display the markdown content as html
     return render(
         request,
-        "encyclopedia/index.html",
-        {"entries": util.list_entries(), "title": title},
+        "encyclopedia/page.html",
+        {"entry": util.get_entry(title), "title": title},
     )
+
+
+def search(request):
+    if request.method == "POST":
+        # Get the form data
+        search_query = request.POST.get("query", "")
+        print("Search query:", search_query)
+        return HttpResponse("Search query:", search_query)
+    # if request.method == "POST":
+    #     search_form = NewSearchForm(request.POST)
+    #     print(search_form)
+    #     if search_form.is_valid():
+    #         search = search_form.cleaned_data["form"]
+    #         display(request, search_form)
+    # elif request.method == "GET":
+    #     search_form = NewSearchForm()
+    #     return render(request, "encyclopedia/layout.html", {"search_form": search_form})
+    # else:
+    #     return HttpResponse("ERROR")
+
+
+# Basic function for debugging as I understand django
+def micla(request):
+    return HttpResponse("MICLA")
